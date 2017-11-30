ee414a08f1556df3b4138cd15fdbb0e6462374c9
htpro <htpro247@gmail.com>
2017-11-30 10:23:38 +0700

    htpro

---
+++ index.js
@@ -0,0 +1,23 @@
+var net = require('net');
+var textChunk = '';
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.on('data', function(data){
+		console.log(data);
+		textChunk = data.toString('utf8');
+		console.log(textChunk);
+		socket.write(textChunk);
+	});
+	socket.on('close', function(){
+		console.log('Closed');
+	});
+});
+server.listen(8080, '127.0.0.1');
