53bc0b2fe321f635b56ae00fc423decb45473379
htpro17 <34116425+htpro17@users.noreply.github.com>
2017-11-30 11:03:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 var net = require('net');
 var textChunk = '';
-
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(process.env.PORT); //the server object listens on port 8080
-
 var server = net.createServer(function(socket) {
 	socket.write('Echo server\r\n');
 	socket.on('data', function(data){
