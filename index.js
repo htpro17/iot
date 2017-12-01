317ee463149baf9d17e4380c2d46a2a8b8f7df60
htpro17 <34116425+htpro17@users.noreply.github.com>
2017-12-02 04:19:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 3484;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+const PORT = 80;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
  
 var http = require('http') 							//#include thư viện http - Tìm thêm về từ khóa http nodejs trên google nếu bạn muốn tìm hiểu thêm. Nhưng theo kinh nghiệm của mình, Javascript trong môi trường NodeJS cực kỳ rộng lớn, khi bạn bí thì nên tìm hiểu không nên ngồi đọc và cố gắng học thuộc hết cái reference (Tài liêu tham khảo) của nodejs làm gì. Vỡ não đó!
 var socketio = require('socket.io')				//#include thư viện socketio
